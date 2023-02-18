f4e559dbe0a52452855fdfe4111696e13ad50bf7
DanilaKotin011 <kotin.02@mail.ru>
2023-02-18 18:41:09 +0700

    add ip to server and format

--- server.cpp
+++ server.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <stdio.h>
 #include <vector>
-#include <sys/types.h> 
-#include <sys/socket.h> 
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <stdio.h>
-#include <netinet/in.h> 
-#include <netdb.h> 
+#include <netinet/in.h>
+#include <netdb.h>
 #include <errno.h>
 #include <strings.h>
 #include <cstring>
@@ -15,58 +15,69 @@
 
 using namespace std;
 
-int main(){
-   struct sockaddr_in servAddr, clientAddr ;
-   char buf[BUFLEN] ;
+int main()
+{
+   struct sockaddr_in servAddr, clientAddr;
+   char buf[BUFLEN];
+   in_addr ip_to_num;
+   int err = 0;
+   int ServSock;
+   unsigned int length;
 
-   int ServSock = socket(AF_INET, SOCK_DGRAM, 0);
-   if (ServSock < 0) {
-		cout << "Error initialization socket"<< endl; 
-		return 1;
-   }
-   else cout << "Server socket initialization is OK" << endl;
-   
-   
-   bzero( (char *) &servAddr, sizeof( servAddr ) );
-   bzero( (char *) &clientAddr, sizeof( clientAddr ) );
-   servAddr.sin_family = AF_INET;
-   servAddr.sin_addr.s_addr = htonl( INADDR_ANY );
-   servAddr.sin_port = 0;
-   
-   if (bind( ServSock, (sockaddr*)&servAddr, sizeof(servAddr)))
+   err = inet_pton(AF_INET, "127.0.0.1", &ip_to_num);
+   if (err < 0)
    {
-	cout<< "Error bind socket"<< endl;
-	return 1;
+      cout << "Error inet_pton" << endl;
+      return 1;
    }
-   else cout<< "Bind os OK"<< endl;
 
-   unsigned int length = sizeof(servAddr);
-   getsockname( ServSock, (sockaddr*)&servAddr, &length);
-   
-   cout<<"Server:"<<endl;
-   cout<<"ip - " << inet_ntoa(servAddr.sin_addr) << endl;
-   cout<<"port - " << ntohs(servAddr.sin_port ) << endl;
-   
-   while(1){
-   	length = sizeof( clientAddr ) ;
-	   bzero( buf, sizeof( BUFLEN) );
-	
-      int res = recvfrom(ServSock,buf,BUFLEN,0,( struct sockaddr *) &clientAddr,&length);
-   	if(res<0){
-   		cout<<"Cant recv"<<buf<<endl;
-   	}
-      else{
-         cout<<"Client: "<<buf<<endl;
-         sendto(ServSock, buf, strlen(buf),0, (const struct sockaddr *) &clientAddr, sizeof(clientAddr));
-         cout<<"ip - " << inet_ntoa(clientAddr.sin_addr) << endl;
-         cout<<"port - " << ntohs(clientAddr.sin_port ) << endl;
-         cout<<endl;
+   ServSock = socket(AF_INET, SOCK_DGRAM, 0);
+   if (ServSock < 0)
+   {
+      cout << "Error initialization socket" << endl;
+      return 1;
+   }
+   else
+      cout << "Server socket initialization is OK" << endl;
+
+   bzero((char *)&servAddr, sizeof(servAddr));
+   bzero((char *)&clientAddr, sizeof(clientAddr));
+   servAddr.sin_family = AF_INET;
+   servAddr.sin_addr = ip_to_num;
+   servAddr.sin_port = 0;
+
+   if (bind(ServSock, (sockaddr *)&servAddr, sizeof(servAddr)))
+   {
+      cout << "Error bind socket" << endl;
+      return 1;
+   }
+   else
+      cout << "Bind os OK" << endl;
+
+   length = sizeof(servAddr);
+   getsockname(ServSock, (sockaddr *)&servAddr, &length);
+
+   cout << "Server:" << endl;
+   cout << "ip - " << inet_ntoa(servAddr.sin_addr) << endl;
+   cout << "port - " << ntohs(servAddr.sin_port) << endl;
+
+   while (1)
+   {
+      length = sizeof(clientAddr);
+      bzero(buf, sizeof(BUFLEN));
+
+      int res = recvfrom(ServSock, buf, BUFLEN, 0, (struct sockaddr *)&clientAddr, &length);
+      if (res < 0)
+      {
+         cout << "Cant recv" << buf << endl;
       }
-   	
-   
+      else
+      {
+         cout << "Client: " << buf << endl;
+         sendto(ServSock, buf, strlen(buf), 0, (const struct sockaddr *)&clientAddr, sizeof(clientAddr));
+         cout << "ip - " << inet_ntoa(clientAddr.sin_addr) << endl;
+         cout << "port - " << ntohs(clientAddr.sin_port) << endl;
+         cout << endl;
+      }
    }
-   
-   
-
-
 }
